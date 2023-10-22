5bea30f58a4578e2a14951579e9a64be8111171a
Alon Spinner <alonspinner@gmail.com>
2023-10-22 22:37:34 +0300

    upp

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,27 @@
+use bevy::prelude::*;
+
 fn main() {
-    println!("Hello, world!");
+    App::new()
+    .add_plugins(DefaultPlugins)
+        .add_systems(Startup, startup)
+        .add_systems(Update, update)
+        .run();
 }
+
+fn startup(
+    mut commands: Commands,
+    asset_server: Res<AssetServer>,) {
+    
+    commands.spawn(Camera2dBundle::default());
+    
+    commands.spawn(SpriteBundle {
+        texture: asset_server.load("background.png"),
+        sprite: Sprite {
+            // custom_size: Some(Vec2::splat(160.0)),
+            ..default()
+        },
+        ..default()
+    });
+    }
+
+fn update() {}
