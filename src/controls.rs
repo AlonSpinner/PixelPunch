2ea2b1b4090fbb9dc6426ea2b896aec746ade943
Alon Spinner <alonspinner@gmail.com>
2023-11-05 20:42:52 +0200

    in the crux

--- src/controls.rs
+++ src/controls.rs
@@ -1,26 +1,41 @@
 use bevy::prelude::*;
 
-pub struct PlayerKeyControl{key : KeyCode, last_press : f64}
+#[derive(Eq, Hash, PartialEq, Clone, Copy)]
+pub enum KeyTarget{
+    Up,
+    Down,
+    Left,
+    Right,
+    Attack,
+    Defend,
+}
+
+#[derive(PartialEq, Clone)]
+pub struct KeyControl{
+    pub keycode : KeyCode,
+    pub keytarget : KeyTarget, 
+    pub last_released : f32
+}
 
 #[derive(Component)]
 pub struct PlayerControls{
-    up : PlayerKeyControl,
-    down : PlayerKeyControl,
-    left : PlayerKeyControl,
-    right : PlayerKeyControl,
-    attack : PlayerKeyControl,
-    defend : PlayerKeyControl,
+    up : KeyControl,
+    down : KeyControl,
+    left : KeyControl,
+    right : KeyControl,
+    attack : KeyControl,
+    defend : KeyControl,
 }
 
 impl Default for PlayerControls {
     fn default() -> Self {
         Self{
-            up : PlayerKeyControl{key: KeyCode::W, last_press: 0.0},
-            down : PlayerKeyControl{key: KeyCode::S, last_press: 0.0},
-            left : PlayerKeyControl{key: KeyCode::A, last_press: 0.0},
-            right : PlayerKeyControl{key: KeyCode::D, last_press: 0.0},
-            attack: PlayerKeyControl{key: KeyCode::F, last_press: 0.0},
-            defend: PlayerKeyControl{key: KeyCode::G, last_press: 0.0},
+            up : KeyControl{keycode: KeyCode::W, keytarget: KeyTarget::Up, last_released: 0.0},
+            down : KeyControl{keycode: KeyCode::S, keytarget: KeyTarget::Down, last_released: 0.0},
+            left : KeyControl{keycode: KeyCode::A, keytarget: KeyTarget::Left, last_released: 0.0},
+            right : KeyControl{keycode: KeyCode::D, keytarget: KeyTarget::Right, last_released: 0.0},
+            attack: KeyControl{keycode: KeyCode::F, keytarget: KeyTarget::Attack, last_released: 0.0},
+            defend: KeyControl{keycode: KeyCode::G, keytarget: KeyTarget::Defend, last_released: 0.0},
         }
     }
 }
--- src/fighters.rs
+++ src/fighters.rs
@@ -1,30 +1,32 @@
 use bevy::prelude::*;
 use strum_macros::{EnumString, Display};
-use super::controls::PlayerControls;
-use std::collections::HashMap;
+use super::controls::{KeyControl,KeyTarget};
+use std::collections::{HashMap,HashSet};
+use std::ops::Add;
 
 #[derive(Component, Clone, Copy, Debug, PartialEq, Eq, Hash, Display)]
 pub enum Fighter{
     IDF,
     HAMAS,
 }
-impl Fighter {
-    pub fn movements(&self) -> Vec<FighterMovement> {
-        match *self {
-            Fighter::IDF => vec!(FighterMovement::Idle,
-                                 FighterMovement::JumpLoop,
-                                 FighterMovement::Docking,
-                                 FighterMovement::Running,
-                                 FighterMovement::Walking),
-            Fighter::HAMAS => vec!(FighterMovement::Idle,
-                                 FighterMovement::JumpLoop,
-                                 FighterMovement::Docking,
-                                 FighterMovement::Running,
-                                 FighterMovement::Walking),
-        }
-    }
+
+pub const IDF_MOVEMENT_GRAPH : FighterMovementGraph = FighterMovementGraph::default();
+pub const HAMAS_MOVEMENT_GRAPH : FighterMovementGraph = FighterMovementGraph::default();
+
+#[derive(Component)]
+pub struct FighterHealth(f32);
+#[derive(Component)]
+pub struct FighterPosition {
+    x : f32,
+    y : f32,
+}
+#[derive(Component)]
+pub struct FighterVelocity {
+    x : f32,
+    y : f32,
 }
 
+//All possible movements for a fighter
 #[derive(Component, Clone, Copy, Debug, PartialEq, Eq, Hash, EnumString, Display)]
 pub enum FighterMovement {
     Idle,
@@ -35,8 +37,8 @@ pub enum FighterMovement {
     Walking,
 }
 impl FighterMovement {
+    //only change if new movement is different to allow Bevy's change detection to work
     pub fn change_to(&mut self, new_movement: FighterMovement) {
-        //only change if new movement is different to allow Bevy's change detection to work
         if &new_movement != self {
             *self = new_movement;
             info!("movement changed to {:?}", self)
@@ -44,46 +46,72 @@ impl FighterMovement {
     }
 }
 
-struct FighterMovementNode {
-    controls : PlayerControls, //combination of controls pressed to enter this node
-    blocked_nodes : Vec<FighterMovement>, //nodes that cannot be entered from this node
+#[derive(Clone)]
+struct FighterMovementNodeTransition {
+    enter_controls : HashSet<KeyControl>, //combination of controls pressed to enter this node
+    enter_condition : fn(position : FighterPosition) -> bool,
+    leave_condition : fn(movement_duration : f32) -> bool,
+}
+impl Default for FighterMovementNodeTransition {
+    fn default() -> Self {
+        Self{
+            enter_controls: HashSet::new(),
+            enter_condition: |position| position.y == 0.0,
+            leave_condition: |movement_duration| true,
+        }
+    }
+}
+impl FighterMovementNodeTransition {
+    fn can_enter(&self, controls : &HashSet<KeyControl>) -> bool {
+        self.enter_controls.is_subset(controls)
+    }
+    fn can_leave(&self, movement_duration : f32) -> bool {
+        (self.leave_condition)(movement_duration)
+    }
 }
 
+#[derive(Clone)]
 struct FighterMovementGraph {
-    nodes : HashMap<FighterMovement,Vec<FighterMovementNode>>,
-    current_node : FighterMovementNode,
+    nodes : HashMap<FighterMovement,FighterMovementNodeTransition>,
+    current_node : FighterMovement,
 }
 
 impl FighterMovementGraph {
     fn default() -> Self {
-        let mut nodes = Vec::new();
-        nodes.push(FighterMovementNode{
-            movement : FighterMovement::Idle,
-            edges : vec!(FighterMovementEdge{
-                            to : FighterMovement::JumpLoop,
-                            controls : PlayerControls::default(),
-                            condition : || true,
-                        }),
-        });
-        nodes.push(FighterMovementNode{
-            movement : FighterMovement::JumpLoop,
-            edges : Vec::new(),
-        });
-        nodes.push(FighterMovementNode{
-            movement : FighterMovement::Docking,
-            edges : Vec::new(),
-        });
-        nodes.push(FighterMovementNode{
-            movement : FighterMovement::Running,
-            edges : Vec::new(),
-        });
-        nodes.push(FighterMovementNode{
-            movement : FighterMovement::Walking,
-            edges : Vec::new(),
-        });
+        let mut nodes = HashMap::new();
+        nodes.insert(FighterMovement::Idle, FighterMovementNodeTransition::default());
+        nodes.insert(FighterMovement::JumpLoop, FighterMovementNodeTransition{
+            enter_controls : HashSet::from([KeyControl{keytarget: KeyTarget::Up, tapped_amount: 1}]),
+            ..default()});
+        nodes.insert(FighterMovement::Docking, FighterMovementNodeTransition{
+            enter_controls : HashSet::from([KeyControl{keytarget: KeyTarget::Down, tapped_amount: 1}]),
+            ..default()});
+        nodes.insert(FighterMovement::Walking, FighterMovementNodeTransition{
+            enter_controls : HashSet::from([KeyControl{keytarget: KeyTarget::Left, tapped_amount: 1},
+                                            KeyControl{keytarget: KeyTarget::Right, tapped_amount: 1}]),
+            ..default()});
+        nodes.insert(FighterMovement::Running, FighterMovementNodeTransition{
+            enter_controls : HashSet::from([KeyControl{keytarget: KeyTarget::Left, tapped_amount: 2},
+                                            KeyControl{keytarget: KeyTarget::Right, tapped_amount: 2}]),
+            ..default()});
         Self{
             nodes,
-            current_node : nodes[0],
+            current_node : FighterMovement::Idle,
+        }
+    }
+    fn movements(&self) -> Vec<FighterMovement> {
+        self.nodes.keys().map(|k| *k).collect()
+    }
+}
+
+impl Add for FighterMovementGraph {
+    type Output = Self;
+    fn add(self, other: Self) -> Self {
+        let mut nodes = self.nodes;
+        nodes.extend(other.nodes);
+        Self{
+            nodes,
+            current_node : self.current_node,
         }
     }
 }
